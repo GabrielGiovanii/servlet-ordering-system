90e81e64757ce08d73f634dc047f316a39ecfedc
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2024-10-11 23:19:55 -0300

    Creating DatabaseConnection class

---
+++ src/main/java/com/servlet_ordering_system/database/DatabaseConnection.java
@@ -0,0 +1,37 @@
+package com.servlet_ordering_system.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Objects;
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/ordering_system";
+    private static final String USER = System.getenv("MYSQL_USER");
+    private static final String PASSWORD = System.getenv("MYSQL_PASSWORD");
+
+    static {
+        try {
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static Connection getConnection()  {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void closeConnection(Connection connection) {
+        try {
+            Objects.requireNonNull(connection).close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
