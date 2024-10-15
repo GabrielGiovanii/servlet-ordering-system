7cd7294b3fe3122be000b4d600a972ef592dba13
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2024-10-15 19:51:51 -0300

    Adding Jackson Datatype library and creating createObjectMapper method in JacksonConfig class

---
+++ src/main/java/com/servlet_ordering_system/config/JacksonConfig.java
@@ -0,0 +1,16 @@
+package com.servlet_ordering_system.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class JacksonConfig {
+
+    public static ObjectMapper createObjectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        return mapper;
+    }
+}
