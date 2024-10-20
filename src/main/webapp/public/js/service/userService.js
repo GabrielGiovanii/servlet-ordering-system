50dcbe1cd9d71a227d6e0517196f83275b8e4fa3
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2024-10-20 19:12:14 -0300

    Creating initial JS version of userService

---
+++ src/main/webapp/public/js/service/userService.js
@@ -0,0 +1,78 @@
+async function saveUser(roleId, insertOrUpdate) {
+    let id = document.querySelector('.modal-dialog #id').value;
+    let name = document.querySelector('.modal-dialog #name').value;
+    let email = document.querySelector('.modal-dialog #email').value;
+    let phone = document.querySelector('.modal-dialog #phone').value;
+    let password = document.querySelector('.modal-dialog #password').value;
+    let confirmPassword = document.querySelector('.modal-dialog #confirmPassword').value;
+
+    if (!name || !email || !phone || !password || !confirmPassword) {
+        showCustomToast("Todos os campos obrigatórios devem ser preenchidos.", "red");
+        return;
+    }
+
+    if (password !== confirmPassword) {
+        showCustomToast("Senha e Confirmação de Senha estão divergentes.", "red");
+        return;
+    }
+
+    let body = {
+        id: id,
+        name: name,
+        email: email,
+        phone: phone,
+        password: password,
+        roleId: roleId
+    };
+
+    if (insertOrUpdate === 'insert') {
+        let response = await insertUser(body);
+        let data = await response.json();
+
+        document.getElementById('id').value = (data.id) ? data.id : "";
+    } else if (insertOrUpdate === 'update') {
+        updateUser(body);
+    }
+}
+
+async function insertUser(body) {
+    try {
+        let response = await makeApiRequest('users', 'POST', body);
+
+        if (response.status === 201) {
+            showCustomToast("Usuário inserido com sucesso.", "green");
+        } else if (response.status === 400) {
+            showCustomToast("Requisição inadequada.", "orange");
+        } else if (response.status === 401) {
+            showCustomToast("Requisição não autorizada.", "red");
+        } else if (response.status === 409) {
+            showCustomToast("Conflito de dados, requisição inadequada.", "red");
+        } else if (response.status === 500) {
+            showCustomToast("Ocorreu um erro inesperado.", "red");
+        }
+
+        return await response;
+    } catch (error) {
+        showCustomToast("Ocorreu um erro inesperado.", "red");
+    }
+}
+
+async function updateUser(body) {
+    try {
+        let response = await makeApiRequest('users', 'PUT', body);
+
+        if (response.status === 200) {
+            showCustomToast("Usuário atualizado com sucesso.", "green");
+        } else if (response.status === 400) {
+            showCustomToast("Requisição inadequada.", "orange");
+        } else if (response.status === 401) {
+            showCustomToast("Requisição não autorizada.", "red");
+        } else if (response.status === 409) {
+            showCustomToast("Conflito de dados, requisição inadequada.", "red");
+        } else if (response.status === 500) {
+            showCustomToast("Ocorreu um erro inesperado.", "red");
+        }
+    } catch (error) {
+        showCustomToast("Ocorreu um erro inesperado.", "red");
+    }
+}
