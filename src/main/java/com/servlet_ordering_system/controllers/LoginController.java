5bfa713d8d55f7777ddbcbacddf8e510f31d43be
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2024-11-10 12:15:44 -0300

    Creating LoginController for the Login view

---
+++ src/main/java/com/servlet_ordering_system/controllers/LoginController.java
@@ -0,0 +1,21 @@
+package com.servlet_ordering_system.controllers;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import static com.servlet_ordering_system.controllers.exceptions.HandlerExceptionController.handleExceptionResponse;
+
+public class LoginController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/login.jsp");
+            dispatcher.forward(req, resp);
+        } catch (Exception e) {
+            handleExceptionResponse(e, resp);
+        }
+    }
+}
