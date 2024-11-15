7b44452b6de9fbd514cd7f7c8630aad16bae645c
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2024-11-15 12:23:53 -0300

    Creating and adjusting initial version of the /manager page

--- src/main/webapp/WEB-INF/views/manager.jsp
+++ src/main/webapp/WEB-INF/views/manager.jsp
@@ -1,10 +1,8 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 
     <%@ include file="common/head.jsp" %>
-            <link rel="stylesheet" href="public/css/screens/home.css">
-            <script src="public/js/screens/home.js" defer></script>
-            <script src="public/js/services/productService.js" defer></script>
-            <script src="public/js/services/categoryService.js" defer></script>
+            <link rel="stylesheet" href="public/css/screens/manager.css">
+            <script src="public/js/screens/manager.js" defer></script>
         </head>
 
         <body>
@@ -20,20 +18,44 @@
                     <div class="col-1">
                       <button class="btn btn-primary" onclick="loadProducts()">Pesquisar</button>
                     </div>
-                    <div class="col-1">
-                      <button class="btn btn-primary" onclick="">Inserir</button>
-                    </div>
                     <div class="col-2">
                       <input type="text" class="form-control" id="productName" placeholder="Nome do produto" maxlength="255">
-                    </div>
-                    <div class="col-2">
-                      <input type="number" class="form-control" id="productId" placeholder="Id do produto" min="1">
                     </div>
                     <div class="col-1">
                       <button class="btn btn-secondary" onclick="cleanProductName()">Limpar</button>
                     </div>
                   </div>
                   <div class="row justify-content-start card-line">
+                  </div>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col">
+                  <div class="row window-title">
+                    <h1>Pedidos</h1>
+                  </div>
+                  <div class="row menu-bar">
+                    <div class="col-1">
+                      <button class="btn btn-primary" onclick="loadOrders()">Pesquisar</button>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <table class="table table-striped" id="orderTable">
+                      <thead>
+                        <tr>
+                          <th class="text-center">Id</th>
+                          <th class="text-center">Id do Cliente</th>
+                          <th class="text-center">Data</th>
+                          <th class="text-center">Status</th>
+                          <th class="text-center">Produto</th>
+                          <th class="text-center">Preço</th>
+                          <th class="text-center">Quantidade</th>
+                          <th class="text-center">Subtotal</th>
+                          <th class="text-center">Total</th>
+                        </tr>
+                      </thead>
+                      <tbody></tbody>
+                    </table>
                   </div>
                 </div>
               </div>
---
+++ src/main/webapp/public/js/screens/manager.js
@@ -0,0 +1,183 @@
+//products
+const products = [
+];
+
+async function loadProducts() {
+    let productContainer = document.querySelector(".card-line");
+    productContainer.innerHTML = '';
+
+    let result = await findProducts();
+    products.length = 0;
+    result.forEach(product => products.push(product));
+
+    products.forEach(product => {
+        let productCardHTML = `
+            <div class="col-3 custom-card">
+                <div class="row">
+                    <div class="col text-center">
+                        <h1 class="fs-5 all-texts">${truncateText(product.name, 28)}</h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col text-center">
+                        <img src="${product.imgUrl}" class="card-img-top custom-img" alt="...">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <p class="text-center fw-bold">
+                        Categoria:
+                        </p>
+                    </div>
+                    <div class="col-6">
+                        <p class="text-center all-texts">${truncateText(product.categoryName, 15)}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <p class="text-center fw-bold">
+                        Preço:
+                        </p>
+                    </div>
+                    <div class="col-6">
+                        <p class="text-center">${formatPrice(product.price)}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <p class="card-description">${truncateText(product.description, 107)}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-5 offset-2">
+                        <button type="button" class="btn btn-primary" onclick="addToCart(${product.id}, this)">Comprar</button>
+                    </div>
+                <div class="col-3">
+                    <input type="number" class="custom-input" min="0" value="0" id="quantityInput">
+                </div>
+                </div>
+            </div>
+        `;
+
+        productContainer.innerHTML += productCardHTML;
+    });
+}
+
+function cleanProductName() {
+    let productNameInput = document.getElementById("productName");
+    productNameInput.value = "";
+}
+
+//orders
+const orders = [];
+
+function addOrderToOrderTable(responseBody) {
+    let orderTableContainer = document.querySelector("#orderTable tbody");
+
+    let buttonInsertedInOrder = false;
+
+    responseBody.orderItems.forEach(item => {
+        let statusName = Object.values(OrderStatus).find(status => status.code === responseBody.orderStatusCode)?.name;
+
+        let orderLine = `
+        <tr>
+            <td id="orderId" class="text-center">${responseBody.id}</td>
+            <td class="text-center">${responseBody.clientId}</td>
+            <td class="text-center">${formatDate(responseBody.moment)}</td>
+            <td id="orderStatusName" class="text-center">${statusName}</td>
+            <td>${item.productName}</td>
+            <td>${formatPrice(item.price)}</td>
+            <td class="text-center">${item.quantity}</td>
+            <td>${formatPrice(item.subtotal)}</td>
+            <td>${formatPrice(responseBody.total)}</td>
+        </tr>
+    `;
+
+    buttonInsertedInOrder = true;
+
+    orderTableContainer.innerHTML += orderLine;
+    });
+}
+
+async function loadOrders() {
+    let orderTableContainer = document.querySelector("#orderTable tbody");
+    orderTableContainer.innerHTML = '';
+
+    let result = await findAllOrders();
+
+    orders.length = 0;
+
+    result.forEach(order => orders.push(order));
+
+    orders.forEach(order => {
+        addOrderToOrderTable(order);
+    });
+}
+
+async function finalizeOrder() {
+    if (cart.length === 0) {
+        showCustomToast("Carrinho vazio. Adicione itens antes de finalizar o pedido.", "orange");
+        return;
+    }
+
+    let orderBody = {
+        orderItems: cart.map(item => ({
+            productId: item.id,
+            quantity: item.quantity
+        }))
+    };
+
+    let responseBody = await insertOrder(orderBody);
+
+    orders.push(responseBody);
+
+    addOrderToOrderTable(responseBody);
+
+    cart.length = 0;
+    document.querySelector("#cartTable tbody").innerHTML = "";
+    document.getElementById("totalAmount").innerHTML = "";
+}
+
+async function cancelOrder(id) {
+    let responseBody = await updateOrder(id);
+
+    let orderTableContainer = document.querySelector("#orderTable tbody");
+
+    let statusName = Object.values(OrderStatus).find(status => status.code === responseBody.orderStatusCode)?.name;
+
+    orderTableContainer.querySelectorAll("tr").forEach(row => {
+        let orderIdElement = row.querySelector("#orderId");
+        
+        if (parseInt(orderIdElement.textContent) === responseBody.id) {
+            let orderStatusNameElement = row.querySelector("#orderStatusName");
+            let orderActionsElement = row.querySelector("#orderActions");
+            
+            orderStatusNameElement.innerHTML = statusName;
+            orderActionsElement.innerHTML = "";
+        }
+    });
+}
+
+async function makePaymentForOrder(orderId) {
+    let paymentMethodSelectElement = document.getElementById("paymentMethodSelect");
+
+    let responseBody = await insertPayment(orderId, paymentMethodSelectElement.value);
+
+    let orderTableContainer = document.querySelector("#orderTable tbody");
+
+    let statusName = Object.values(OrderStatus).find(status => status.code === 2)?.name;
+
+    orderTableContainer.querySelectorAll("tr").forEach(row => {
+        let orderIdElement = row.querySelector("#orderId");
+
+        if (parseInt(orderIdElement.textContent) === responseBody.orderId) {
+            let orderStatusNameElement = row.querySelector("#orderStatusName");
+            let orderActionsElement = row.querySelector("#orderActions");
+
+            orderStatusNameElement.innerHTML = statusName;
+            orderActionsElement.innerHTML = "";
+
+            document.querySelector('#registerModal .btn-secondary').click();
+        }
+    });
+}
