eaceebbf1b9571507ccd7da427ba1979c250d0a8
Gabriel Giovani <gabrielgiovanesouza32@gmail.com>
2024-11-15 11:00:06 -0300

    Adjusting makeApiRequest method to handle HTTP 204 status as success

--- src/main/webapp/public/js/services/util/requestService.js
+++ src/main/webapp/public/js/services/util/requestService.js
@@ -15,24 +15,25 @@ async function makeApiRequest(resource, method, body, showSuccessMessage) {
         }
 
         let response = await fetch(endpoint, requestOptions);
+        let httpStatus = response.status;
 
-        if (response.status === 200 || response.status === 201) {
+        if (httpStatus === 200 || httpStatus === 201 || httpStatus === 204) {
             if (showSuccessMessage) {
                 showCustomToast("Sucesso ao realizar ação.", "green");
             }
 
             return response;
-        } else if (response.status === 400) {
+        } else if (httpStatus === 400) {
             showCustomToast(`Requisição inadequada para o recurso: '${resource}' e o método: '${method}'.`, "orange");
-        } else if (response.status === 401) {
+        } else if (httpStatus === 401) {
             if (resource === 'auth') {
                 showCustomToast("E-mail ou senha incorretos ou usuário inexistente.", "orange");
             } else {
                 showCustomToast(`Requisição não autorizada para o recurso: '${resource}' e o método: '${method}'.`, "orange");
             }
-        } else if (response.status === 409) {
+        } else if (httpStatus === 409) {
             showCustomToast(`Requisição com conflito de dados para o recurso: '${resource}' e o método: '${method}'.`, "red");
-        } else if (response.status === 500) {
+        } else if (httpStatus === 500) {
             showCustomToast(`Erro inesperado no servidor para o recurso: '${resource}' e o método: '${method}'.`, "red");
         } else {
             showCustomToast(`Ocorreu algo inesperado no recurso: '${resource}' e o método: '${method}'.`, "red");
